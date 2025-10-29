3efb939989e92831127e84f734d73a1c39205ef9
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-10-29 22:09:21 +0700

    rm max request

--- src/service/model-types.ts
+++ src/service/model-types.ts
@@ -3,7 +3,6 @@ export interface ModelOption {
   name: string;
   category: string;
   model: string;
-  max_request: string;
 }
 
 export const MODEL_OPTIONS: ModelOption[] = [
@@ -12,49 +11,42 @@ export const MODEL_OPTIONS: ModelOption[] = [
     id: "gpt-5",
     name: "GPT 5",
     model: "openai/gpt-oss-20b:free",
-    max_request: "1000",
   },
   {
     category: "AI Coding Assistant Pintar",
     id: "claude-sonnet-4",
     name: "Claude Sonnet 4",
     model: "meituan/longcat-flash-chat:free",
-    max_request: "1000",
   },
   {
     category: "Refactor Besar / File Panjang",
     id: "glm-air",
     name: "GLM 4.5 Air",
     model: "z-ai/glm-4.5-air:free",
-    max_request: "1000",
   },
   {
     category: "Live Coding Cepat, Responsif",
     id: "gemini-2.5-pro",
     name: "Gemini 2.5 Pro",
     model: "minimax/minimax-m2:free",
-    max_request: "1000",
   },
   {
     category: "Refactor Besar / File Panjang",
     id: "deepseek-v3",
     name: "Deepseek V3",
-    model: "deepseek/deepseek-r1-0528:free",
-    max_request: "1000",
+    model: "deepseek/deepseek-chat-v3.1:free",
   },
   {
     category: "Refactor Besar / File Panjang",
     id: "qween-a255",
     name: "Qween 3 Coder A235",
     model: "qwen/qwen3-coder:free",
-    max_request: "1000",
   },
   {
     category: "Live Coding Cepat, Responsif",
     id: "llama-4",
     name: "LLaMA 4",
     model: "meta-llama/llama-4-maverick:free",
-    max_request: "1000",
   },
 ];
 
